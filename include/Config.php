12dcadd0b76f0efb1610d4889b310c4f2322b2ec
Hardik Patel <hardikpatel0884@gmail.com>
2018-06-21 21:27:47 +0530

    initial commit

---
+++ include/Config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'alfacoaching');
+define('DB_PASSWORD', 'Har884dik');
+define('DB_HOST', '182.50.133.89');
+define('DB_NAME', 'alfacoaching');
+
+define('USER_CREATED_SUCCESSFULLY', 0);
+define('USER_CREATE_FAILED', 1);
+define('USER_ALREADY_EXISTED', 2);
+?>
